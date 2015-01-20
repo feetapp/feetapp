9eaaabc9010f9ac5a3d2a9f4132c623a25852e66
featapp <featapp@gmail.com>
2015-01-20 13:44:30 +0300

    ExtJS 5.1

---
+++ src/main/webapp/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('feetapp.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'feetapp'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
---
+++ src/main/webapp/build/temp/production/feetapp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /home/user/IdeaProjects/feetapp/src/main/webapp/app.js
